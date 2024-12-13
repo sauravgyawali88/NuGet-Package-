07ca6391f5eab1ad4dcc3dbc6820ec87a682650e
sauravgyawali88 <gyawalisaurav892@gmail.com>
2024-12-13 04:18:55 +0000

    created class with a method

---
+++ LibraryProject/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryProject
+{
+    // A simple class that provides functionality for printing messages to the console
+    public class Class1
+    {
+        // Method to print a predefined message to the console
+        public void PrintMessage()
+        {
+            // Output a message to the console
+            Console.WriteLine("Greetings from MyNuGetPackage!");
+        }
+    }
+}
